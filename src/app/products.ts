18985edf4ad602d86b891782f665e1a1ccec9a18
GiuseppeLanucara <giuseppelanucara96@gmail.com>
2023-03-07 12:30:19 +0100

    MODIFIED PRODUCTS NAME

--- src/app/products.ts
+++ src/app/products.ts
@@ -8,19 +8,19 @@ export interface Product {
 export const products = [
   {
     id: 1,
-    name: 'Phone XL',
-    price: 799,
-    description: 'A large phone with one of the best screens'
+    name: 'Xbox Series X',
+    price: 499,
+    description: 'The most powerful Console'
   },
   {
     id: 2,
-    name: 'Phone Mini',
-    price: 699,
-    description: 'A great phone with one of the best cameras'
+    name: 'Xbox Series S and X Bundle',
+    price: 899,
+    description: 'Take all the family in a exclusive bundle'
   },
   {
     id: 3,
-    name: 'Phone Standard',
+    name: 'Xbox Series S',
     price: 299,
     description: ''
   }
